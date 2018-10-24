8ab5e122afc9c0d43c5c2cac54ee02d8d8ccdd0a
chenjie <4234115@qq.com>
2018-10-24 09:55:15 +0800

    Initial Commit

---
+++ IPADownload/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  IPADownload
+//
+//  Created by Jason on 2018/10/24.
+//  Copyright © 2018 Jason. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ IPADownload/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  IPADownload
+//
+//  Created by Jason on 2018/10/24.
+//  Copyright © 2018 Jason. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
